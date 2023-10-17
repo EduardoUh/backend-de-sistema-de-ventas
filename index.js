b162a1d27133085ff84c8e76fc2d63d75d06c741
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-10-17 10:42:31 -0600

    feat(sucursales): add route to create a new sucursal, also middleware to check if it already exists

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 const { connectDb } = require('./db/config.js');
-const { authRouter } = require('./routes/index.js');
+const { authRouter, sucursalesRouter } = require('./routes/index.js');
 
 const app = express();
 
@@ -15,6 +15,7 @@ const startServer = async () => {
     app.use(express.json());
 
     app.use('/api/v1', authRouter);
+    app.use('/api/v1', sucursalesRouter);
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/public/index.html'));
