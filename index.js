058e42e625d3bafabcc9aa59ffe468c3ccbe3363
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-11-02 14:53:25 -0600

    feat(roles): add route and controller logic to get the roles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 const { connectDb } = require('./db/config.js');
-const { authRouter, sucursalesRouter, usuariosRouter } = require('./routes/index.js');
+const { authRouter, sucursalesRouter, usuariosRouter, rolesRouter } = require('./routes/index.js');
 
 const app = express();
 
@@ -17,6 +17,7 @@ const startServer = async () => {
     app.use('/api/v1', authRouter);
     app.use('/api/v1', sucursalesRouter);
     app.use('/api/v1', usuariosRouter);
+    app.use('/api/v1', rolesRouter);
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/public/index.html'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const { authRouter } = require('./auth.js');
 const { sucursalesRouter } = require('./sucursales.js');
 const { usuariosRouter } = require('./usuarios.js');
+const { rolesRouter } = require('./roles.js');
 
 module.exports = {
     authRouter,
     sucursalesRouter,
-    usuariosRouter
+    usuariosRouter,
+    rolesRouter
 }
