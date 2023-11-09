268dc7d2b449d72d454582160932f0a4f8851029
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-11-09 11:17:50 -0600

    feat(proveedores): add route and controller logic to update a proveedor by id

--- controllers/proveedores.js
+++ controllers/proveedores.js
@@ -1,4 +1,4 @@
-const { request, response } = require('express');
+const { request, response, json } = require('express');
 const { Proveedor } = require('../models/index.js');
 
 
@@ -27,6 +27,44 @@ module.exports.crearProveedor = async (req = request, res = response) => {
         res.status(500).json({
             ok: false,
             message: 'Algo salió mal al crear el proveedor, intente de nuevo y si el fallo persiste contacte al administrador'
+        });
+    }
+}
+
+module.exports.actualizarProveedor = async (req = request, res = response) => {
+    const { nombre, direccion, numTelefono, email, rfc } = req.body;
+    const { id: proveedorId } = req.params;
+
+    try {
+        const proveedor = await Proveedor.findById(proveedorId);
+
+        if (!proveedor) {
+            return res.status(404).json({
+                ok: false,
+                message: "Proveedor inexistente"
+            });
+        }
+
+        await proveedor.updateOne({ nombre, direccion, numTelefono, email, rfc });
+
+        res.status(200).json({
+            ok: true,
+            message: `Proveedor ${nombre} actualizado correctamente`
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        if (error.code === 11000) {
+            return res.status(409).json({
+                ok: false,
+                message: "Ya existe un proveedor registrado con ese email o rfc"
+            });
+        }
+
+        res.status(500).json({
+            ok: false,
+            message: "Algo salió mal al actualizar el proveedor, intente de nuevo y si el fallo persiste contacte al administrador"
         });
     }
 }
