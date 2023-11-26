fe2a0acd990c6d48f742708fdc8a5b16aeb43b3d
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-11-26 13:44:32 -0600

    feat(ventas-pagos): add models for ventas and pagos
    refactor(ventas-pagos): add the pagoCon field to the Ventas and Pagos models

---
+++ models/Ventas.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose');
+
+
+const ventaSchema = new Schema({
+    sucursal: {
+        type: Schema.Types.ObjectId,
+        ref: 'Sucursales',
+        required: true
+    },
+    creador: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuarios',
+        required: true
+    },
+    cliente: {
+        type: Schema.Types.ObjectId,
+        ref: 'Clientes',
+        default: null
+    },
+    articulos: [{
+        producto: {
+            type: Schema.Types.ObjectId,
+            ref: 'Productos',
+            required: true
+        },
+        cantidad: {
+            type: Number,
+            required: true
+        }
+    }],
+    total: {
+        type: Number,
+        required: true
+    },
+    pagoCon: {
+        type: Number,
+        required: true
+    },
+    pago: {
+        type: Number,
+        required: true
+    },
+    cambio: {
+        type: Number,
+        required: true
+    },
+    saldo: {
+        type: Number,
+        required: true
+    },
+    saldada: {
+        type: Boolean,
+        required: true
+    },
+    fechaCreacion: {
+        type: Number,
+        required: true
+    }
+});
+
+ventaSchema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+module.exports.Venta = model('Ventas', ventaSchema);
