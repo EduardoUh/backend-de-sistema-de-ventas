3978d91e936a550dca40c31a79641db04705df4a
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-05 15:52:42 -0600

    refactor(proveedores): add session to the creation and update logic, add the created and updated proveedor to the response

--- controllers/proveedores.js
+++ controllers/proveedores.js
@@ -10,6 +10,8 @@ module.exports.crearProveedor = async (req = request, res = response) => {
     let session = null;
 
     try {
+        session = await startSession();
+
         const proveedorYaExiste = await Proveedor.findOne().or([{ rfc }, { email }]).exec();
 
         if (proveedorYaExiste) {
@@ -20,8 +22,6 @@ module.exports.crearProveedor = async (req = request, res = response) => {
         }
 
         const proveedor = new Proveedor({ nombre, direccion, numTelefono, email, rfc, creador: uId, fechaCreacion: Date.now(), ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() });
-
-        session = await startSession();
 
         session.startTransaction();
 
@@ -58,11 +58,13 @@ module.exports.crearProveedor = async (req = request, res = response) => {
         res.status(201).json({
             ok: true,
             message: `Proveedor ${nombre} creado con éxito`,
-            proveedorCreado
+            proveedor: proveedorCreado
         });
 
     } catch (error) {
-        await session.abortTransaction();
+        if (session?.transaction?.isActive) {
+            await session.abortTransaction();
+        }
 
         console.log(error);
 
@@ -72,46 +74,95 @@ module.exports.crearProveedor = async (req = request, res = response) => {
         });
     }
     finally {
-        await session.endSession();
+        if (session) {
+            await session.endSession();
+        }
     }
 }
 
 module.exports.actualizarProveedor = async (req = request, res = response) => {
-    const { nombre, direccion, numTelefono, email, rfc } = req.body;
+    const { nombre, direccion, numTelefono, email, rfc, activo } = req.body;
     const { id: proveedorId } = req.params;
     const { uId } = req;
+    let session = null;
 
     try {
-        const proveedor = await Proveedor.findById(proveedorId);
+        session = await startSession();
+
+        const [proveedor, proveedorEmail, proveedorRfc] = await Promise.all([
+            Proveedor.findById(proveedorId),
+            Proveedor.findOne({ email }),
+            Proveedor.findOne({ rfc })
+        ]);
 
         if (!proveedor) {
             return res.status(404).json({
                 ok: false,
-                message: 'Proveedor inexistente'
+                message: 'Proveedor no encontrado'
             });
         }
 
-        await proveedor.updateOne({ nombre, direccion, numTelefono, email, rfc, ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() });
+        if (proveedorEmail && proveedor.id !== proveedorEmail.id || proveedorRfc && proveedor.id !== proveedorRfc.id) {
+            return res.status(409).json({
+                ok: false,
+                message: 'Ya existe un proveedor con ése email o rfc'
+            });
+        }
+
+        session.startTransaction();
+
+        await proveedor.updateOne({ nombre, direccion, numTelefono, email, rfc, activo, ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() }).session(session);
+
+        const proveedorActualizado = await Proveedor.findById(proveedor.id)
+            .populate({
+                path: 'creador',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            })
+            .populate({
+                path: 'ultimoEnModificar',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            }).session(session);
+
+        await session.commitTransaction();
 
         res.status(200).json({
             ok: true,
-            message: `Proveedor ${nombre} actualizado correctamente`
+            message: `Proveedor ${nombre} actualizado correctamente`,
+            proveedor: proveedorActualizado
         });
 
     } catch (error) {
-        console.log(error);
-
-        if (error.code === 11000) {
-            return res.status(409).json({
-                ok: false,
-                message: 'Ya existe un proveedor registrado con ese email o rfc'
-            });
+        if (session?.transaction?.isActive) {
+            await session.abortTransaction();
         }
+
+        console.log(error);
 
         res.status(500).json({
             ok: false,
             message: 'Algo salió mal al actualizar el proveedor, intente de nuevo y si el fallo persiste contacte al administrador'
         });
+    }
+    finally {
+        if (session) {
+            await session.endSession();
+        }
     }
 }
 
