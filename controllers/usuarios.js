1e95b30b4b03ac98e873ef7141adfc640244f61c
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-04 23:46:24 -0600

    refactor(usuarios): change the update routes and controller logic, add the created and updated user to the response

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,13 +1,18 @@
 const { request, response } = require('express');
+const { startSession } = require('mongoose');
 const { Usuario, Rol, Sucursal } = require('../models/index.js');
 const { hash, compare } = require('bcrypt');
-const { transformarDatosPopulateRol, transformarDatosPopulatedSucursal, filtrarQueryParams } = require('../helpers/index.js');
+const { transformarDatosPopulateRol, transformarDatosPopulatedSucursal, transformarDatosPopulatedUsuario, filtrarQueryParams } = require('../helpers/index.js');
 
 
 module.exports.crearUsuario = async (req = request, res = response) => {
     const { body, esAdministrador, sucursalUsuario } = req;
+    const { uId } = req;
+    let session = null;
 
     try {
+        session = await startSession();
+
         const promises = [Rol.findById(body.rol)];
 
         if (body.sucursal) {
@@ -57,16 +62,64 @@ module.exports.crearUsuario = async (req = request, res = response) => {
 
         body.password = await hash(body.password, 12);
 
-        const usuario = new Usuario(body);
+        const usuario = new Usuario({ ...body, creador: uId, fechaCreacion: Date.now(), ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() });
 
-        await usuario.save();
+        session.startTransaction();
+
+        await usuario.save({ session });
+
+        const usuarioCreado = await Usuario.findById(usuario.id)
+            .select('-password')
+            .populate({
+                path: 'sucursal',
+                options: {
+                    transform: transformarDatosPopulatedSucursal
+                }
+            })
+            .populate({
+                path: 'rol',
+                options: {
+                    transform: transformarDatosPopulateRol
+                }
+            })
+            .populate({
+                path: 'creador',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            })
+            .populate({
+                path: 'ultimoEnModificar',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            }).session(session);
+
+        await session.commitTransaction();
 
         res.status(201).json({
             ok: true,
-            message: `Usuario ${body.nombres} creado con éxito`
+            message: `Usuario ${body.nombres} creado con éxito`,
+            usuario: usuarioCreado
         });
 
     } catch (error) {
+        if (session.transaction.isActive) {
+            await session.abortTransaction();
+        }
+
         console.log(error);
 
         res.status(500).json({
@@ -74,147 +127,28 @@ module.exports.crearUsuario = async (req = request, res = response) => {
             message: 'Algo salió mal al crear el usuario, intente de nuevo y si el fallo persiste contacte al administrador'
         })
     }
+    finally {
+        if (session) {
+            await session.endSession();
+        }
+    }
 }
 
-module.exports.actualizarPerfilAdminsVendedores = async (req = request, res = response) => {
-    const { nombres, apellidoPaterno, apellidoMaterno, email, password, direccion, numTelefono } = req.body;
+module.exports.actualizarMiPerfil = async (req = request, res = response) => {
+    const { nombres, apellidoPaterno, apellidoMaterno, rfc, email, password, direccion, numTelefono } = req.body;
     const { id } = req.params;
-    const { uId, esAdministrador, esVendedor } = req;
-    let hashedPassword;
+    const { uId } = req;
+    let hashedPassword = null;
 
     try {
-        if (uId !== id || !esAdministrador && !esVendedor) {
+        if (uId !== id) {
             return res.status(401).json({
                 ok: false,
-                message: 'Sin las credenciales necesarias para realizar ésta acción'
+                message: 'Solo puedes actualizar tu usuario'
             });
         }
 
-        const usuario = await Usuario.findById(uId);
-
-        if (password && typeof password !== 'string' || password && password.length < 5) {
-            return res.status(400).json({
-                ok: false,
-                message: 'La contraseña debe ser una cadena de texto con al menos cinco caracteres'
-            });
-        }
-
-        if (password) {
-            hashedPassword = await hash(password, 12);
-        } else {
-            hashedPassword = usuario.password;
-        }
-
-        await usuario.updateOne({ nombres, apellidoPaterno, apellidoMaterno, email, password: hashedPassword, direccion, numTelefono });
-
-        res.status(200).json({
-            ok: true,
-            message: `Perfil actualizado correctamente`
-        });
-
-    } catch (error) {
-        console.log(error);
-
-        if (error.code === 11000) {
-            return res.status(409).json({
-                ok: false,
-                message: 'Ya existe un usuario con ése email'
-            });
-        }
-
-        res.status(500).json({
-            ok: false,
-            message: 'Algo salió mal al actualizar el perfil, intente de nuevo y si el fallo persiste contacte al administrador'
-        });
-    }
-}
-
-module.exports.adminActualizaDatosVendedor = async (req = request, res = response) => {
-    const { nombres, apellidoPaterno, apellidoMaterno, rfc, email, password, direccion, numTelefono, activo } = req.body;
-    const { sucursalUsuario } = req;
-    const { id: idVendedor } = req.params;
-    let hashedPassword;
-
-    try {
-        const vendedor = await Usuario.findById(idVendedor)
-            .populate({
-                path: 'rol',
-                options: {
-                    transform: transformarDatosPopulateRol
-                }
-            });
-
-        if (!vendedor) {
-            return res.status(404).json({
-                ok: false,
-                message: 'Vendedor no encontrado'
-            });
-        }
-
-        if (vendedor.rol !== 'VENDEDOR') {
-            return res.status(401).json({
-                ok: false,
-                message: 'Sin las credenciales necesarias para actualizar éste usuario'
-            });
-        }
-
-        if (sucursalUsuario !== vendedor.sucursal.toHexString()) {
-            return res.status(401).json({
-                ok: false,
-                message: 'Sin las credenciales necesarias para actualizar éste vendedor'
-            });
-        }
-
-        if (password && typeof password !== 'string' || password && password.length < 5) {
-            return res.status(400).json({
-                ok: false,
-                message: 'La contraseña debe ser una cadena de texto con al menos cinco caracteres'
-            });
-        }
-
-        if (password) {
-            hashedPassword = await hash(password, 12);
-        } else {
-            hashedPassword = vendedor.password;
-        }
-
-        await vendedor.updateOne({ nombres, apellidoPaterno, apellidoMaterno, rfc, email, password: hashedPassword, direccion, numTelefono, activo });
-
-        res.status(200).json({
-            ok: true,
-            message: `Vendedor ${nombres} actualizado correctamente`
-        });
-
-    } catch (error) {
-        console.log(error);
-
-        if (error.code === 11000) {
-            return res.status(409).json({
-                ok: false,
-                message: 'Ya existe un usuario con ese rfc o email'
-            });
-        }
-
-        res.status(500).json({
-            ok: false,
-            message: 'Algo salió mal al actualizar el vendedor, intente de nuevo y si el fallo persiste contacte al administrador'
-        });
-    }
-}
-
-module.exports.actualizarDatosAdminsVendedores = async (req = request, res = response) => {
-    const { nombres, apellidoPaterno, apellidoMaterno, rfc, rol, sucursal, email, password, direccion, numTelefono, activo } = req.body;
-    const { id: idUsuario } = req.params;
-    let hashedPassword;
-
-    try {
-        const promises = [
-            Usuario.findById(idUsuario),
-            Rol.findById(rol),
-            Sucursal.findById(sucursal)
-        ];
-
-        const [usuario, dbRol, dbSucursal] = await Promise.all(promises);
+        const [usuario, usuarioEmail, usuarioRfc] = await Promise.all([Usuario.findById(id), Usuario.findOne({ email }), Usuario.findOne({ rfc })]);
 
         if (!usuario) {
             return res.status(404).json({
@@ -223,49 +157,29 @@ module.exports.actualizarDatosAdminsVendedores = async (req = request, res = res
             });
         }
 
-        if (!dbRol) {
-            return res.status(401).json({
+        if (usuarioEmail && usuario.id !== usuarioEmail.id || usuarioRfc && usuario.id !== usuarioRfc.id) {
+            return res.status(409).json({
                 ok: false,
-                message: 'Rol inválido'
-            });
-        }
-
-        if (!dbSucursal) {
-            return res.status(401).json({
-                ok: false,
-                message: 'Sucursal Inválida'
-            });
-        }
-
-        if (password && typeof password !== 'string' || password && password.length < 5) {
-            return res.status(400).json({
-                ok: false,
-                message: 'La contraseña debe ser una cadena de texto con al menos cinco caracteres'
+                message: 'Ya existe un usuario con ése email o rfc'
             });
         }
 
         if (password) {
             hashedPassword = await hash(password, 12);
-        } else {
+        }
+        else {
             hashedPassword = usuario.password;
         }
 
-        await usuario.updateOne({ nombres, apellidoPaterno, apellidoMaterno, rfc, rol, sucursal: dbRol.rol === 'SUPER USUARIO' ? null : sucursal, email, password: hashedPassword, direccion, numTelefono, activo });
+        await usuario.updateOne({ nombres, apellidoPaterno, apellidoMaterno, rfc, email, password: hashedPassword, direccion, numTelefono, ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() });
 
         res.status(200).json({
             ok: true,
-            message: `${dbRol.rol} ${nombres} actualizado correctamente`
+            message: 'Has actualizado tu información correctamente'
         });
 
     } catch (error) {
         console.log(error);
-
-        if (error.code === 11000) {
-            return res.status(409).json({
-                ok: false,
-                message: 'Ya existe un usuario con ese rfc o email'
-            });
-        }
 
         res.status(500).json({
             ok: false,
@@ -274,63 +188,135 @@ module.exports.actualizarDatosAdminsVendedores = async (req = request, res = res
     }
 }
 
-module.exports.actualizarPerfilSuperUsuario = async (req = request, res = response) => {
-    const { nombres, apellidoPaterno, apellidoMaterno, rfc, email, password, direccion, numTelefono } = req.body;
-    const { id: idUsuario } = req.params;
-    const { uId } = req;
-    let hashedPassword;
+module.exports.actualizarOtrosPerfiles = async (req = request, res = response) => {
+    const { nombres, apellidoPaterno, apellidoMaterno, rfc, rol, sucursal, email, password, direccion, numTelefono, activo } = req.body;
+    const { id: usuarioId } = req.params;
+    const { uId, esAdministrador, sucursalUsuario } = req;
+    let session = null;
+    let hashedPassword = null;
 
     try {
-        if (uId !== idUsuario) {
+        session = await startSession();
+
+        if (esAdministrador && sucursalUsuario !== sucursal) {
             return res.status(401).json({
                 ok: false,
-                message: 'Sin las credenciales para actualizar ése usuairo'
+                message: 'Sin las credenciales para asignar ésa sucursal'
             });
         }
 
-        const usuario = await Usuario.findById(idUsuario);
+        const [usuario, usuarioEmail, usuarioRfc, rolDb, sucursalDb] = await Promise.all([
+            Usuario.findById(usuarioId)
+                .populate('rol'),
+            Usuario.findOne({ email }),
+            Usuario.findOne({ rfc }),
+            Rol.findById(rol),
+            Sucursal.findById(sucursal)
+        ]);
 
-        if (!usuario) {
+        if (!usuario || !rolDb || !sucursalDb) {
             return res.status(404).json({
                 ok: false,
-                message: 'Usuario inexistente'
+                message: 'Usuario, Sucursal o rol no encontrados'
             });
         }
 
-        if (password && typeof password !== 'string' || password && password.length < 5) {
-            return res.status(400).json({
+        if (esAdministrador && usuario.rol.rol !== 'VENDEDOR' || esAdministrador && usuario.sucursal.toHexString() !== sucursalUsuario) {
+            return res.status(401).json({
                 ok: false,
-                message: 'La contraseña debe ser una cadena de texto con al menos cinco caracteres'
+                message: 'Sin las credenciales necesarias para actualizar ése usuario'
+            });
+        }
+
+        if (esAdministrador && rolDb.rol !== 'VENDEDOR') {
+            return res.status(401).json({
+                ok: false,
+                message: 'Sin las credenciales necesarias para asignar ése rol'
+            });
+        }
+
+        if (usuarioEmail && usuario.id !== usuarioEmail.id || usuarioRfc && usuario.id !== usuarioRfc.id) {
+            return res.status(409).json({
+                ok: false,
+                message: 'Ya existe un usuario con ése email o rfc'
             });
         }
 
         if (password) {
             hashedPassword = await hash(password, 12);
-        } else {
+        }
+        else {
             hashedPassword = usuario.password;
         }
 
-        await usuario.updateOne({ nombres, apellidoPaterno, apellidoMaterno, rfc, email, password: hashedPassword, direccion, numTelefono });
+        session.startTransaction();
+
+        await usuario.updateOne({ nombres, apellidoPaterno, apellidoMaterno, rfc, rol, sucursal: rolDb.rol === 'SUPER USUARIO' ? null : sucursal, email, password: hashedPassword, direccion, numTelefono, activo, ultimoEnModificar: uId, fechaUltimaModificacion: Date.now() })
+            .session(session);
+
+        const usuarioActualizado = await Usuario.findById(usuario.id)
+            .select('-password')
+            .populate({
+                path: 'sucursal',
+                options: {
+                    transform: transformarDatosPopulatedSucursal
+                }
+            })
+            .populate({
+                path: 'rol',
+                options: {
+                    transform: transformarDatosPopulateRol
+                }
+            })
+            .populate({
+                path: 'creador',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            })
+            .populate({
+                path: 'ultimoEnModificar',
+                options: {
+                    transform: transformarDatosPopulatedUsuario
+                },
+                populate: {
+                    path: 'rol',
+                    options: {
+                        transform: transformarDatosPopulateRol
+                    }
+                }
+            }).session(session);
+
+        await session.commitTransaction();
 
         res.status(200).json({
             ok: true,
-            message: `Super usuario ${nombres} actualizado correctamente`
+            message: `Usuario ${nombres} actualizado correctamente`,
+            usuario: usuarioActualizado
         });
 
     } catch (error) {
-        console.log(error);
-
-        if (error.code === 11000) {
-            return res.status(409).json({
-                ok: false,
-                message: 'Ya existe un usuario con ese rfc o email'
-            });
+        if (session.transaction.isActive) {
+            await session.abortTransaction();
         }
+
+        console.log(error);
 
         res.status(500).json({
             ok: false,
-            message: 'Algo salió mal al actualizar los datos, intente de nuevo y si el fallo persiste contacte al administrador'
+            message: 'Algo salió mal al actualizar el usuario, intente de nuevo y si el fallo persiste contacte al administrador'
         });
+    }
+    finally {
+        if (session) {
+            await session.endSession();
+        }
     }
 }
 
