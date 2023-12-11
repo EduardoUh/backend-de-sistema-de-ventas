5c275cc2ac4254f9b7b70bd6f152f492470eb357
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-11 12:17:58 -0600

    fix(middlewares): change the logical or operator for the and operator because it was causing credentials bugs

--- middlewares/verificarPermisos/verificarPermisosModuloProveedores.js
+++ middlewares/verificarPermisos/verificarPermisosModuloProveedores.js
@@ -30,7 +30,7 @@ module.exports.verificarPermisosModuloProveedores = (req = request, res = respon
             });
         }
 
-        if (method === 'GET' && moduloProveedores && !moduloProveedores.permisos.find(permiso => permiso === 'VER') || method === 'GET' && moduloProductos && !moduloProductos.permisos.find(permiso => permiso === 'CREAR') || method === 'GET' && moduloCrearCompra && !moduloCrearCompra.permisos.find(permiso => permiso === 'CREAR')) {
+        if ((method === 'GET' && moduloProveedores && !moduloProveedores.permisos.find(permiso => permiso === 'VER')) && (method === 'GET' && moduloProductos && !moduloProductos.permisos.find(permiso => permiso === 'CREAR')) && (method === 'GET' && moduloCrearCompra && !moduloCrearCompra.permisos.find(permiso => permiso === 'CREAR'))) {
             return res.status(401).json({
                 ok: false,
                 message: 'Sin las credenciales necesarias para ésta acción'
